cb774507d375ff505fbfc36386c880eb2d1d2a97
Luis Salazar Manzano <lesamz@umich.edu>
2024-07-23 17:06:32 -0400

    updating lightcurve class

--- tdkit/lightcurve.py
+++ tdkit/lightcurve.py
@@ -1,5 +1,15 @@
+import numpy as np
+from astropy.time import Time
+
 class LightCurve:
-    def __init__(self, time, flux, fluxerr):
-        self.time = time
-        self.flux = flux
-        self.flux_err = fluxerr
+    def __init__(self, time, mag, magerr=None):
+        if isinstance(time, Time):
+            self.time = time
+        else:
+            raise TypeError('time must be an astropy Time object')
+        
+        self.mag = mag
+        if magerr is None:
+            self.magerr = np.zeros_like(mag)
+        else:
+            self.magerr = magerr
